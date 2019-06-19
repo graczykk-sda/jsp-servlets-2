d2f1584a0a6e14251081fbda0caf1b0f49784382
graczykk <krzysztof.graczyk@tutanota.com>
2019-06-19 23:15:06 +0200

    hello servlets initial commit

---
+++ helloservlets/src/main/java/pl/sdacademy/jsp/servlets/HelloServlet.java
@@ -0,0 +1,18 @@
+package pl.sdacademy.jsp.servlets;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "hello-servlet",
+        urlPatterns = {"/hello"})
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.println("Hello Servlet.");
+    }
+}
